23efbf83b5ce47b58d5441c0fc2007559949ded2
Ivan Seredkin <ivan.seredkin@gmail.com>
2020-01-11 01:04:35 +0300

    Ready to deploy to heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { connect } = require('mongoose');
 const bodyParser = require('body-parser');
 const { mongodbUri } = require('./config/key');
 const { now } = require('./utils');
+const path = require('path');
 
 const items = require('./routes/api/v1/items');
 
@@ -21,6 +22,15 @@ connect(mongodbUri, {
 
 // Use Routes
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Run server
 const port = process.env.PORT || 5000;
