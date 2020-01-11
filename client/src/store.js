44bc80baaa5b6214815f95eee48ab8a57323908f
Ivan Seredkin <ivan.seredkin@gmail.com>
2020-01-11 15:42:38 +0300

    Store bug fixed

--- client/src/store.js
+++ client/src/store.js
@@ -8,7 +8,7 @@ const middleware = [thunk];
 
 const store = createStore(rootReducer, initialState, compose(
   applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() || compose()
 ));
 
 export default store;
